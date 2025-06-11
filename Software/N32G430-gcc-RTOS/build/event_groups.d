61fac09a5682d6ee50a72c561c3c425cb6350d2f
BrookDawn <gjjsay5120@gmail.com>
2025-06-11 17:57:05 +0800

    添加了FreeRTOS的工程框架

---
+++ Software/N32G430-gcc-RTOS/build/event_groups.d
@@ -0,0 +1,70 @@
+build/event_groups.o: FreeRTOS/Src/event_groups.c FreeRTOS/Inc/FreeRTOS.h \
+ core/inc/FreeRTOSConfig.h CMSIS/device/n32g430.h CMSIS/core/core_cm4.h \
+ CMSIS/core/cmsis_version.h CMSIS/core/cmsis_compiler.h \
+ CMSIS/core/cmsis_gcc.h CMSIS/device/system_n32g430.h \
+ CMSIS/device/n32g430_conf.h n32g430_std_periph_driver/inc/n32g430_adc.h \
+ n32g430_std_periph_driver/inc/n32g430_can.h \
+ n32g430_std_periph_driver/inc/n32g430_comp.h \
+ n32g430_std_periph_driver/inc/n32g430_crc.h \
+ n32g430_std_periph_driver/inc/n32g430_dbg.h \
+ n32g430_std_periph_driver/inc/n32g430_dma.h \
+ n32g430_std_periph_driver/inc/n32g430_exti.h \
+ n32g430_std_periph_driver/inc/n32g430_flash.h \
+ n32g430_std_periph_driver/inc/n32g430_gpio.h \
+ n32g430_std_periph_driver/inc/n32g430_i2c.h \
+ n32g430_std_periph_driver/inc/n32g430_iwdg.h \
+ n32g430_std_periph_driver/inc/n32g430_pwr.h \
+ n32g430_std_periph_driver/inc/n32g430_rcc.h \
+ n32g430_std_periph_driver/inc/n32g430_rtc.h \
+ n32g430_std_periph_driver/inc/n32g430_spi.h \
+ n32g430_std_periph_driver/inc/n32g430_tim.h \
+ n32g430_std_periph_driver/inc/n32g430_usart.h \
+ n32g430_std_periph_driver/inc/n32g430_wwdg.h \
+ n32g430_std_periph_driver/inc/n32g430_beeper.h \
+ n32g430_std_periph_driver/inc/misc.h CMSIS/device/system_n32g430.h \
+ FreeRTOS/Inc/projdefs.h FreeRTOS/Inc/portable.h \
+ FreeRTOS/Inc/deprecated_definitions.h \
+ FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h FreeRTOS/Inc/mpu_wrappers.h \
+ FreeRTOS/Inc/task.h FreeRTOS/Inc/list.h FreeRTOS/Inc/timers.h \
+ FreeRTOS/Inc/task.h FreeRTOS/Inc/event_groups.h FreeRTOS/Inc/timers.h
+FreeRTOS/Inc/FreeRTOS.h:
+core/inc/FreeRTOSConfig.h:
+CMSIS/device/n32g430.h:
+CMSIS/core/core_cm4.h:
+CMSIS/core/cmsis_version.h:
+CMSIS/core/cmsis_compiler.h:
+CMSIS/core/cmsis_gcc.h:
+CMSIS/device/system_n32g430.h:
+CMSIS/device/n32g430_conf.h:
+n32g430_std_periph_driver/inc/n32g430_adc.h:
+n32g430_std_periph_driver/inc/n32g430_can.h:
+n32g430_std_periph_driver/inc/n32g430_comp.h:
+n32g430_std_periph_driver/inc/n32g430_crc.h:
+n32g430_std_periph_driver/inc/n32g430_dbg.h:
+n32g430_std_periph_driver/inc/n32g430_dma.h:
+n32g430_std_periph_driver/inc/n32g430_exti.h:
+n32g430_std_periph_driver/inc/n32g430_flash.h:
+n32g430_std_periph_driver/inc/n32g430_gpio.h:
+n32g430_std_periph_driver/inc/n32g430_i2c.h:
+n32g430_std_periph_driver/inc/n32g430_iwdg.h:
+n32g430_std_periph_driver/inc/n32g430_pwr.h:
+n32g430_std_periph_driver/inc/n32g430_rcc.h:
+n32g430_std_periph_driver/inc/n32g430_rtc.h:
+n32g430_std_periph_driver/inc/n32g430_spi.h:
+n32g430_std_periph_driver/inc/n32g430_tim.h:
+n32g430_std_periph_driver/inc/n32g430_usart.h:
+n32g430_std_periph_driver/inc/n32g430_wwdg.h:
+n32g430_std_periph_driver/inc/n32g430_beeper.h:
+n32g430_std_periph_driver/inc/misc.h:
+CMSIS/device/system_n32g430.h:
+FreeRTOS/Inc/projdefs.h:
+FreeRTOS/Inc/portable.h:
+FreeRTOS/Inc/deprecated_definitions.h:
+FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+FreeRTOS/Inc/mpu_wrappers.h:
+FreeRTOS/Inc/task.h:
+FreeRTOS/Inc/list.h:
+FreeRTOS/Inc/timers.h:
+FreeRTOS/Inc/task.h:
+FreeRTOS/Inc/event_groups.h:
+FreeRTOS/Inc/timers.h:
---
+++ Software/N32G430-gcc-RTOS/core/inc/n32g430_conf.h
@@ -0,0 +1,37 @@
+/**
+*\*\file n32g430_conf.h
+*\*\author Nations
+*\*\version v1.0.0
+*\*\copyright Copyright (c) 2022, Nations Technologies Inc. All rights reserved.
+**/ 
+
+#ifndef __N32G430_CONF_H__
+#define __N32G430_CONF_H__
+
+/* Uncomment/Comment the line below to enable/disable peripheral header file inclusion */
+
+#include "n32g430_adc.h"
+#include "n32g430_can.h"
+#include "n32g430_comp.h"
+#include "n32g430_crc.h"
+#include "n32g430_dbg.h"
+#include "n32g430_dma.h"
+#include "n32g430_exti.h"
+#include "n32g430_flash.h"
+#include "n32g430_gpio.h"
+#include "n32g430_i2c.h"
+#include "n32g430_iwdg.h"
+#include "n32g430_pwr.h"
+#include "n32g430_rcc.h"
+#include "n32g430_rtc.h"
+#include "n32g430_spi.h"
+#include "n32g430_tim.h"
+#include "n32g430_usart.h"
+#include "n32g430_wwdg.h"
+#include "n32g430_iwdg.h"
+#include "n32g430_beeper.h"
+
+#include "misc.h" /* High level functions for NVIC and SysTick (add-on to CMSIS functions) */
+
+
+#endif /* __N32G430_CONF_H__ */ 
