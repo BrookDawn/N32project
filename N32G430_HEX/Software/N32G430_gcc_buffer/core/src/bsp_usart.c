32271e0c47b98f76df6b3723ae9ff33f8486bf01
BrookDawn <gjjsay5120@gmail.com>
2025-12-09 14:51:59 +0800

    添加 USART2 驱动和 Modbus 主站功能

--- N32G430_HEX/Software/N32G430_gcc_buffer/core/src/bsp_usart.c
+++ N32G430_HEX/Software/N32G430_gcc_buffer/core/src/bsp_usart.c
@@ -213,3 +213,95 @@ void USART1_IRQHandler(void)
         }
     }
 }
+
+/**
+ *\*\name   USART2_Init.
+ *\*\fun    Initialize USART2 for Modbus master.
+ *\*\param  none.
+ *\*\return none.
+**/
+void USART2_Init(void)
+{
+    GPIO_InitType GPIO_InitStructure;
+    USART_InitType USART_InitStructure;
+
+    RCC_AHB_Peripheral_Clock_Enable(USART2_GPIO_CLK);
+    RCC_APB1_Peripheral_Clock_Enable(USART2_CLK);
+
+    /* TX: PA2 */
+    GPIO_InitStructure.Pin              = USART2_TX_PIN;
+    GPIO_InitStructure.GPIO_Mode        = GPIO_MODE_AF_PP;
+    GPIO_InitStructure.GPIO_Pull        = GPIO_NO_PULL;
+    GPIO_InitStructure.GPIO_Slew_Rate   = GPIO_SLEW_RATE_FAST;
+    GPIO_InitStructure.GPIO_Current     = GPIO_DS_8MA;
+    GPIO_InitStructure.GPIO_Alternate   = USART2_GPIO_AF;
+    GPIO_Peripheral_Initialize(USART2_GPIO_PORT, &GPIO_InitStructure);
+
+    /* RX: PA3 */
+    GPIO_InitStructure.Pin              = USART2_RX_PIN;
+    GPIO_InitStructure.GPIO_Mode        = GPIO_MODE_INPUT;
+    GPIO_InitStructure.GPIO_Pull        = GPIO_NO_PULL;
+    GPIO_InitStructure.GPIO_Slew_Rate   = GPIO_SLEW_RATE_FAST;
+    GPIO_InitStructure.GPIO_Current     = GPIO_DS_8MA;
+    GPIO_InitStructure.GPIO_Alternate   = USART2_GPIO_AF;
+    GPIO_Peripheral_Initialize(USART2_GPIO_PORT, &GPIO_InitStructure);
+
+    USART_InitStructure.BaudRate        = USART2_BAUDRATE;
+    USART_InitStructure.WordLength      = USART2_WORDLENGTH;
+    USART_InitStructure.StopBits        = USART2_STOPBITS;
+    USART_InitStructure.Parity          = USART2_PARITY;
+    USART_InitStructure.HardwareFlowControl = USART2_HARDWAREFLOWCTL;
+    USART_InitStructure.Mode            = USART2_MODE;
+    USART_Initializes(USART2, &USART_InitStructure);
+
+    USART_Enable(USART2);
+    USART2_FlushRxBuffer();
+}
+
+void USART2_SendByte(uint8_t data)
+{
+    while(USART_Flag_Status_Get(USART2, USART_FLAG_TXDE) == RESET)
+    {
+    }
+    USART_Data_Send(USART2, data);
+}
+
+void USART2_SendArray(const uint8_t* data, uint16_t length)
+{
+    for(uint16_t i = 0; i < length; i++)
+    {
+        USART2_SendByte(data[i]);
+    }
+}
+
+bool USART2_ReadByteTimeout(uint8_t* data, uint32_t timeout_ms)
+{
+    if(data == NULL)
+    {
+        return false;
+    }
+
+    do
+    {
+        if(USART_Flag_Status_Get(USART2, USART_FLAG_RXDNE) != RESET)
+        {
+            *data = (uint8_t)USART_Data_Receive(USART2);
+            return true;
+        }
+        if(timeout_ms == 0)
+        {
+            break;
+        }
+        SysTick_Delay_Ms(1);
+    } while(timeout_ms-- > 0);
+
+    return false;
+}
+
+void USART2_FlushRxBuffer(void)
+{
+    while(USART_Flag_Status_Get(USART2, USART_FLAG_RXDNE) != RESET)
+    {
+        (void)USART_Data_Receive(USART2);
+    }
+}
